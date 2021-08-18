50abaad750d9eab12b0b8502d02c7ed921ce62ba
Egoy Magnaye <webdev@egoymagnaye.com>
2021-08-18 16:27:13 +0800

    Initial project setup

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
+module.exports = {
+  entry: {
+    ["sample"]: ["./src/js/sample.js", "./src/sass/style.scss"],
+  },
+  output: {
+    path: path.resolve(
+      "../fao-local/wp-content/themes/em-theme-hello/assets/",
+      "js"
+    ), //__dirname
+    filename: "[name].js",
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "../css/styles.css",
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new UglifyJsPlugin(), new CssMinimizerPlugin()],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] },
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader?url=false",
+          {
+            loader: "sass-loader",
+            options: {
+              sassOptions: {
+                sourceMap: true,
+                outputStyle: "expanded",
+              },
+            },
+          },
+          "postcss-loader",
+        ],
+      },
+    ],
+  },
+};
